0ecbcabceb29d3d38951dbcce09e394c5027d5c0
DevAhmadYarKhan <210438969+DevAhmadYarKhan@users.noreply.github.com>
2026-04-24 17:42:12 +0100

    Have Flask read and apply config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,7 @@
+from config import Config   # Import the Config class from the config module
 from flask import Flask # Import the Flask class from the flask package.
 
 app = Flask(__name__) # Initialise app as an object of type Flask.
+app.config.from_object(Config)  # Apply the configurations defined by Config class
 
 from app import routes # Imported at bottom to avoid circular import.
