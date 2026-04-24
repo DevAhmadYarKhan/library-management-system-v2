e7d7891aeccbb77a4e4c2ea93ec42815d09e3cef
DevAhmadYarKhan <210438969+DevAhmadYarKhan@users.noreply.github.com>
2026-04-24 20:12:12 +0100

    Fix typo in routes.py

--- app/routes.py
+++ app/routes.py
@@ -11,5 +11,5 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect(url_for('/index'))
+        return redirect(url_for('index'))
     return render_template('login.html', title='Log In', form=form)
