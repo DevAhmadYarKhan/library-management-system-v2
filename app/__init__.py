0e1ab99107f6cbd0f70cd207c6138f7b251b1da1
DevAhmadYarKhan <210438969+DevAhmadYarKhan@users.noreply.github.com>
2026-04-24 01:15:49 +0100

    Comment circular import workaround

--- app/__init__.py
+++ app/__init__.py
@@ -2,4 +2,4 @@ from flask import Flask # Import the Flask class from the flask package.
 
 app = Flask(__name__) # Initialise app as an object of type Flask.
 
-from app import routes
+from app import routes # Imported at bottom to avoid circular import.
