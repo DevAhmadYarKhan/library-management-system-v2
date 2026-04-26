e0bed4ddf8f8256097f15b1a30679c220ed18c9c
DevAhmadYarKhan <210438969+DevAhmadYarKhan@users.noreply.github.com>
2026-04-26 22:00:48 +0100

    Create User table model for database

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from config import Config   # Import the Config class from the config module
 from flask import Flask # Import the Flask class from the flask package.
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__) # Initialise app as an object of type Flask.
 app.config.from_object(Config)  # Apply the configurations defined by Config class
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-from app import routes # Imported at bottom to avoid circular import.
+from app import routes, models # Imported at bottom to avoid circular import.
