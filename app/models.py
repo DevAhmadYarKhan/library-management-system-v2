3693f9002ac2f16e01c4295a8c97f056f9a1ff84
DevAhmadYarKhan <210438969+DevAhmadYarKhan@users.noreply.github.com>
2026-04-27 18:06:03 +0100

    Add 'Member' table to database

--- app/models.py
+++ app/models.py
@@ -11,3 +11,11 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+class Member(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    name: so.Mapped[str] = so.mapped_column(sa.String(64), index=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
+
+    def __repr__(self):
+        return '<Member {}>'.format(self.email)
